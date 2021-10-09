1e35f2238d1b77b2fb50d25928762e2c76602d1e
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-09 15:37:03 +0530

    mapped all pages

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {MenuList} from './MenuList'
 
 const Navbar = () => {
+
+    const menuList = MenuList.map(({url, title}, index) => {
+        return (
+            <li key={index}>
+                <a href={url}>{title}</a>
+            </li>
+        )
+    });
     return(
         <div>
-            Navbar
+            <div className="logo">
+            VPN<font>Lab</font>
+            </div>
+            <ul className="menu-list">
+                 {menuList}
+            </ul>
         </div>
     )
 }
