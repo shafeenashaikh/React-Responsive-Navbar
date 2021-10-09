9e9676016dddf1322cc877a15c3a17bb27a8f0d1
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-09 19:32:47 +0530

    made Navbar responsiv

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {MenuList} from './MenuList'
 import "./Navbar.css";
 import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
 
+    const[clicked,setClicked] = useState(false)
     const menuList = MenuList.map(({url, title}, index) => {
         return (
             <li key={index}>
@@ -14,15 +15,19 @@ const Navbar = () => {
             </li>
         )
     });
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
     return(
         <nav>
             <div className="logo">
             VPN<font>Lab</font>
             </div>
-            <div className="menu-icon">
-                <i className="fa fa-bars"></i>
+            <div className="menu-icon" onClick={handleClick}>
+                <i className={clicked ? "fas fa-times": "fas fa-bars"}></i>
             </div>
-            <ul className="menu-list">
+            <ul className={clicked ? "menu-list" : "menu-list close"}>
                  {menuList}
             </ul>
         </nav>
