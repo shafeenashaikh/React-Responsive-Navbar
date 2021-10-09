03f329d7a0682603d7cfea41e93981013030b68f
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-09 13:16:51 +0530

    created MenuList

---
+++ src/Components/Navbar/MenuList.js
@@ -0,0 +1,22 @@
+export const MenuList =[
+    {
+        title:"Home",
+        url:"/",
+    },
+    {
+        title:"Key Features",
+        url:"/fetures",
+    },
+    {
+        title:"Pricing",
+        url:"/pricing",
+    },
+    {
+        title:"Testimonials",
+        url:"/tesimonials"
+    },
+    {
+        title:"Try For Free",
+        url:"/demo"
+    },
+]
